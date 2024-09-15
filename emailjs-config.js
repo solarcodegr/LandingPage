73f87f51f3aebb2a3a96b7b9a481988282a476ea
aggelako <48869882+aggelako@users.noreply.github.com>
2024-09-15 15:15:35 +0000

    fixed social media linkes

--- emailjs-config.js
+++ emailjs-config.js
@@ -1,5 +1,7 @@
 const EMAILJS_CONFIG = {
-  publicKey: "rMhFVg4AaWPLBKvSv",      // Replace with your EmailJS public key
-  serviceID: "service_dz49bkg",      // Replace with your EmailJS service ID
-  templateID: "template_8cizz7x"     // Replace with your EmailJS template ID
+  publicKey: "rMhFVg4AaWPLBKvSv",      
+  serviceID: "service_dz49bkg",     
+  templateID: "template_8cizz7x"     
 };
+
+//if you find this pls dont spam :)
