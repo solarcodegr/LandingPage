fc9f13637cd0150c1bc92ece9ab8d72abd47ce5c
Dimitrios Makris <dim.makris2000@gmail.com>
2024-09-23 18:52:20 +0300

    feat: contact form has all fields mandatory, alerts was made prettier

--- form-handler.js
+++ form-handler.js
@@ -2,12 +2,16 @@
 // emailjs-config.js must be loaded before this script in your HTML file
 
 // Initialize EmailJS with the public key from the configuration
+
 (function(){
     emailjs.init(EMAILJS_CONFIG.publicKey);
 })();
 
 // Add event listener to the send button
-document.getElementById('send-button').addEventListener('click', function() {
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+
+    event.preventDefault(); // Prevent the default form submission
+
     // Get the form values
     const firstName = document.getElementById('first-name').value;
     const lastName = document.getElementById('last-name').value;
@@ -22,9 +26,41 @@ document.getElementById('send-button').addEventListener('click', function() {
         message: description
     }).then(function(response) {
         console.log('SUCCESS!', response.status, response.text);
-        alert("Your message has been sent!");
+
+        // Display success message with SweetAlert2
+        Swal.fire({
+            icon: 'success',
+            title: 'Message Sent!',
+            text: 'Your message has been sent successfully.',
+            confirmButtonText: 'OK',
+            background: 'white',
+            color: '#06044B',
+            customClass: {
+                title: 'geologica-font',
+                popup: 'geologica-font',
+                confirmButton: 'swal2-custom-confirm-button',
+            }
+        });
+
+        // Clear the form fields
+        document.getElementById('contact-form').reset();
+
     }, function(error) {
         console.log('FAILED...', error);
-        alert("Failed to send the message. Please try again later.");
+        // alert("Failed to send the message. Please try again later.");
+
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Failed to send the message. Please try again later.',
+            confirmButtonText: 'OK',
+            background: 'white',
+            color: '#06044B',
+            customClass: {
+                title: 'geologica-font',
+                popup: 'geologica-font',
+                confirmButton: 'swal2-custom-confirm-button',
+            }
+        });
     });
 });
